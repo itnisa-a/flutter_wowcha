3fe15da68ccbefbd894a28693a008f2d75ab7a8b
Icanfly37 <101133532+Icanfly37@users.noreply.github.com>
2023-09-02 22:29:48 +0700

    Add Old Fu*king code Here

---
+++ project/lib/Friend/modules/component/Header_web.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+AppBar NavBar(){
+  return AppBar(
+        toolbarHeight: 60,
+        title: const Text(
+          'ระบบจัดตารางสอนอาจารย์ มหาวิทยาลัยเกษตรศาสตร์',
+          style: TextStyle(
+            fontSize: 16,
+            color: Color.fromRGBO(0, 102, 94, 1),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: const Color.fromRGBO(195, 231, 196, 1),
+      );
+}
